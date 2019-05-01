6d2e0e2b7e6678cab618ad8c936d2a97f40197e7
Nikita Sementcov <kasabiankpp@gmail.com>
2019-05-01 17:05:49 +0200

    Install prettier. Config it. Add prettier plugin to eslint. Format code

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: [
-    `${__dirname}/src/index.jsx`,
-  ],
+  entry: [`${__dirname}/src/index.jsx`],
   externals: {
     gon: 'gon',
   },

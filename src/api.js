552941912ba008c83393c3269b898541c3e7a132
Nikita Sementcov <kasabiankpp@gmail.com>
2019-05-06 20:55:26 +0200

    Add API and constants modules

---
+++ src/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+import { apiUrl } from './consts';
+
+function configAxios() {
+  axios.defaults.baseURL = apiUrl;
+  axios.defaults.timeout = 1000;
+}
+
+configAxios();
+
+const postMessage = (channelId, message) =>
+  axios.post(`channels/${channelId}/messages`, {
+    data: { attributes: { message } },
+  });
+
+export default postMessage;

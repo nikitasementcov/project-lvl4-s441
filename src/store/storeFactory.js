9e69219f1e948710c24310b7b0f894f8b16b157f
Nikita Sementcov <kasabiankpp@gmail.com>
2019-05-06 22:16:08 +0200

    Add redux devtools extension and redux-thunk as store middlewares

--- src/store/storeFactory.js
+++ src/store/storeFactory.js
@@ -1,4 +1,6 @@
-import { createStore } from 'redux';
+/* eslint-disable no-underscore-dangle */
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
@@ -14,10 +16,16 @@ export const mapChannels = channels => {
   );
 };
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const buildStore = gon => {
   const { channels } = gon;
   const preloadedState = { domain: { channels: mapChannels(channels) } };
-  return createStore(rootReducer, preloadedState);
+  return createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(applyMiddleware(thunk))
+  );
 };
 
 export default buildStore;

e66eed8cd60052ef15f395a9b1a56f81257d05f8
Nikita Sementcov <kasabiankpp@gmail.com>
2019-05-09 19:13:37 +0200

    Add message displaying (without preloading)

--- src/components/MessagesList/MessagesList.jsx
+++ src/components/MessagesList/MessagesList.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import cn from 'classnames';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
-export default class MessagesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+import Message from '../Message/Message';
 
+const mapStateToProps = state => ({
+  messages: state.domain.messages.byId, // TODO: selector
+});
+
+@connect(mapStateToProps)
+class MessagesList extends Component {
   render() {
-    const { className } = this.props;
+    const { className, messages } = this.props;
     const classes = cn(className);
-    return <div className={classes} />;
+    return (
+      <div className={classes}>
+        {_.map(messages, m => (
+          <Message key={m.id} message={m} />
+        ))}
+      </div>
+    );
   }
 }
+export default MessagesList;

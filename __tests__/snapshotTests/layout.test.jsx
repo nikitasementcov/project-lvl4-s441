4e8a5108f5669a3dbb63722c1113e2f7c3e14517
NikitaSementcov <kasabiankpp@gmail.com>
2019-06-23 22:54:46 +0200

    add snapshop test of layout

---
+++ __tests__/snapshotTests/layout.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import Layout from '../../src/components/Layout/Layout';
+
+jest.mock('../../src/components/AlertModal/AlertModal', () => () => <div />);
+
+it('renders correctly', () => {
+  const mockComponent = () => <></>;
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <Layout path="/" component={mockComponent} />
+      </MemoryRouter>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});

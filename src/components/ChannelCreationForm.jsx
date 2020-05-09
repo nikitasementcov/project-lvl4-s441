5b01e63861cc3398a9eb17328212ec5af2b532ca
Nikita Sementcov <sementcov.nikita@gmail.com>
2020-05-09 23:33:31 +0200

    migrate channelCreationForm to the hooks

--- src/components/ChannelCreationForm.jsx
+++ src/components/ChannelCreationForm.jsx
@@ -1,50 +1,37 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Button, Form, Label } from 'reactstrap';
 
 import { addChannel } from '../store/channels';
 
-const mapDispatchToProps = dispatch => ({
-  handleChannelCreation: name => dispatch(addChannel(name)),
-});
-
-@reduxForm({ form: 'channelCreation' })
-@connect(null, mapDispatchToProps)
-class ChannelCreationForm extends React.Component {
-  handleSubmit = async ({ name }) => {
-    const { handleChannelCreation, reset } = this.props;
-    await handleChannelCreation(name);
+const ChannelCreationForm = ({ reset, handleSubmit }) => {
+  const dispatch = useDispatch();
+  const submit = async ({ name }) => {
+    await dispatch(addChannel(name));
     reset();
   };
 
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <Form
-        inline
-        onSubmit={handleSubmit(this.handleSubmit)}
-        className="position-sticky"
+  return (
+    <Form inline onSubmit={handleSubmit(submit)} className="position-sticky">
+      <Label for="name">Create new channel:</Label>
+      <Field
+        name="name"
+        component="input"
+        type="text"
+        className="form-control w-100 my-2"
+        placeholder="Name"
+      />
+      <Button
+        outline
+        color="primary"
+        type="submit"
+        className="new-channel-button col-md-12 "
       >
-        <Label for="name">Create new channel:</Label>
-        <Field
-          name="name"
-          component="input"
-          type="text"
-          className="form-control w-100 my-2"
-          placeholder="Name"
-        />
-        <Button
-          outline
-          color="primary"
-          type="submit"
-          className="new-channel-button col-md-12 "
-        >
-          Create
-        </Button>
-      </Form>
-    );
-  }
-}
+        Create
+      </Button>
+    </Form>
+  );
+};
 
-export default ChannelCreationForm;
+export default reduxForm({ form: 'channelCreation' })(ChannelCreationForm);
